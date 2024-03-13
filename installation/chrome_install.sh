1145307b930283ae463ee829407184398fe9f3d0
Paul Becker <p@becker.kiwi>
2024-03-13 01:28:43 +0100

    cleanup

--- installation/chrome_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo zypper refresh
-sudo zypper -n install curl
-sudo rpm --import https://dl.google.com/linux/linux_signing_key.pub
-sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
-sudo zypper install -n chrome-browser
