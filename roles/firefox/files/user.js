4103f1437d80b96486533be1b4f173af526e3666
Paul Becker <p@becker.kiwi>
2024-09-29 22:23:00 +0200

    minimal firefox css

--- roles/firefox/files/user.js
+++ roles/firefox/files/user.js
@@ -31,7 +31,6 @@ user_pref("network.http.max-connections", 1800); // default=900
 user_pref("browser.translations.neverTranslateLanguages", "de, en")
 user_pref("browser.translations.panelShown", false)
 user_pref("network.http.max-persistent-connections-per-server", 10); // default=6; download connections; anything above 10 is excessive
-
 user_pref("general.smoothScroll",                                       true); // DEFAULT
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
 user_pref("general.smoothScroll.msdPhysics.enabled",                    true);
@@ -39,14 +38,54 @@ user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",  600);
 user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",      650);
 user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         25);
 user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",      2.0);
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     250);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     25
 user_pref("general.smoothScroll.currentVelocityWeighting",              1.0);
 user_pref("general.smoothScroll.stopDecelerationWeighting",             1.0);
 user_pref("mousewheel.default.delta_multiplier_y",                      300); // 250-400
-
 user_pref("distribution.openSUSE.bookmarksProcessed", false);
 user_pref("app.partner.opensuse", "");
 user_pref("toolkit.aboutProcesses.showProfilerIcons", false);
 user_pref("privacy.donottrackheader.enabled", true);
-user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.main.tools", "syncedtabs,history");
+user_pref("browser.ml.chat.sidebar", false);
 user_pref("extensions.pocket.enabled", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false); 
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false); 
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false); 
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false); 
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false); 
+user_pref("toolkit.telemetry.coverage.opt-out", true); 
+user_pref("toolkit.coverage.opt-out", true); 
+user_pref("browser.send_pings", false);
+user_pref("browser.send_pings.require_same_host", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("browser.display.use_document_fonts", 1);
+user_pref("distribution.openSUSE.bookmarksProcessed", false);
+user_pref("browser.translations.neverTranslateLanguages", "de, en");
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
+user_pref("privacy.trackingprotection.cryptomining.enabled", true);
+user_pref("geo.enabled", false);
+user_pref("dom.private-attribution.submission.enabled", false);
+user_pref("media.eme.enabled", true); 
+user_pref("media.gmp-widevinecdm.enabled", true);
+user_pref("browser.cache.memory.capacity", 512000); 
+user_pref("network.trr.mode", 5);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines", "");
+user_pref("browser.vpn_promo.enabled", false);
