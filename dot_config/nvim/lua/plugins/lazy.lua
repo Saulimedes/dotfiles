86b7c58207020d7fc0b4449005fcff2dad65031d
Paul Becker <50241811+Saulimedes@users.noreply.github.com>
2025-12-24 23:14:13 +0100

    remove textobjects

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -106,69 +106,31 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
       "JoosepAlviste/nvim-ts-context-commentstring",
     },
     config = function()
-      -- Wait a bit for treesitter to be ready
-      vim.defer_fn(function()
-        pcall(function()
-          require("nvim-treesitter.configs").setup({
-            ensure_installed = {"c", "python", "perl", "lua", "css", "html", "rust", "yaml", "markdown", "toml", "bash", "jq", "json", "javascript", "typescript" },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-              enable = true,
-              disable = { "markdown" },
-              additional_vim_regex_highlighting = false,
+      pcall(function()
+        require("nvim-treesitter.configs").setup({
+          ensure_installed = {"c", "python", "perl", "lua", "css", "html", "rust", "yaml", "markdown", "toml", "bash", "jq", "json", "javascript", "typescript" },
+          sync_install = false,
+          auto_install = true,
+          highlight = {
+            enable = true,
+            disable = { "markdown" },
+            additional_vim_regex_highlighting = false,
+          },
+          indent = { enable = true },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = "<CR>",
+              node_incremental = "<CR>",
+              scope_incremental = "<S-CR>",
+              node_decremental = "<BS>",
             },
-            indent = { enable = true },
-            incremental_selection = {
-              enable = true,
-              keymaps = {
-                init_selection = "<CR>",
-                node_incremental = "<CR>",
-                scope_incremental = "<S-CR>",
-                node_decremental = "<BS>",
-              },
-            },
-            textobjects = {
-              select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                  ["af"] = "@function.outer",
-                  ["if"] = "@function.inner",
-                  ["ac"] = "@class.outer",
-                  ["ic"] = "@class.inner",
-                  ["aa"] = "@parameter.outer",
-                  ["ia"] = "@parameter.inner",
-                },
-              },
-              move = {
-                enable = true,
-                set_jumps = true,
-                goto_next_start = {
-                  ["]f"] = "@function.outer",
-                  ["]c"] = "@class.outer",
-                },
-                goto_next_end = {
-                  ["]F"] = "@function.outer",
-                  ["]C"] = "@class.outer",
-                },
-                goto_previous_start = {
-                  ["[f"] = "@function.outer",
-                  ["[c"] = "@class.outer",
-                },
-                goto_previous_end = {
-                  ["[F"] = "@function.outer",
-                  ["[C"] = "@class.outer",
-                },
-              },
-            },
-          })
-        end)
-      end, 200)
+          },
+        })
+      end)
     end,
   },
 
