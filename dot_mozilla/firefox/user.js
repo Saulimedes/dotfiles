162683e5b3a0a61760374d44c8c91fcf01c295a7
Paul Becker <50241811+Saulimedes@users.noreply.github.com>
2026-02-05 23:08:03 +0100

    add firefox defaults

--- dot_mozilla/firefox/user.js
+++ dot_mozilla/firefox/user.js
@@ -1,2 +1,67 @@
-// Firefox user.js — managed by chezmoi
-// Add your preferences below
+user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("browser.urlbar.speculativeConnect.enabled", true);
+user_pref("browser.places.speculativeConnect.enabled", true);
+user_pref("network.prefetch-next", true);
+user_pref("network.http.max-persistent-connections-per-server", 20); // increase download connections
+user_pref("privacy.trackingprotection.allow_list.convenience.enabled", false); // disable Strict allowlist of convenience features
+user_pref("signon.rememberSignons", false); // disable password manager
+user_pref("extensions.formautofill.addresses.enabled", false); // disable address manager
+user_pref("extensions.formautofill.creditCards.enabled", false); // disable credit card manager
+user_pref("browser.urlbar.suggest.recentsearches", false);  // unselect "Show recent searches" for clean UI
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false); // unselect "Show search suggestions ahead of browsing history in address bar results" for clean UI
+user_pref("signon.management.page.breach-alerts.enabled", false); // extra hardening
+user_pref("signon.generation.enabled", false); // unselect "Suggest and generate strong passwords" for clean UI
+user_pref("signon.firefoxRelay.feature", ""); // unselect suggestions from Firefox Relay for clean UI
+user_pref("browser.safebrowsing.downloads.enabled", false); // deny SB to scan downloads to identify suspicious files; local checks only
+user_pref("browser.safebrowsing.downloads.remote.url", ""); // enforce no remote checks for downloads by SB
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false); // clean up UI; not needed in user.js if remote downloads are disabled
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false); // clean up UI; not needed in user.js if remote downloads are disabled
+user_pref("browser.safebrowsing.allowOverride", false); // do not allow user to override SB
+user_pref("browser.search.update", false); // do not update opensearch engines
+user_pref("network.trr.confirmationNS", "skip"); // skip TRR confirmation request
+user_pref("extensions.webextensions.restrictedDomains", ""); // remove Mozilla domains so adblocker works on pages
+user_pref("browser.firefox-view.feature-tour", "{\"screen\":\"\",\"complete\":true}"); // disable the Firefox View tour from popping up for new profiles
+user_pref("accessibility.force_disabled", 1); // disable Accessibility features
+user_pref("security.cert_pinning.enforcement_level", 2); // strict public key pinning
+user_pref("captivedetect.canonicalURL", ""); // disable captive portal detection
+user_pref("network.captive-portal-service.enabled", false); // disable captive portal detection
+user_pref("network.connectivity-service.enabled", false); // disable captive portal detection
+user_pref("browser.download.enableDeletePrivate", true); // Delete files downloaded in private browsing when all private windows are closed
+user_pref("browser.download.deletePrivateChosen", true); // Delete files downloaded in private browsing when all private windows are closed
+user_pref("browser.download.deletePrivate", true); // Delete files downloaded in private browsing when all private windows are closed
+user_pref("devtools.accessibility.enabled", false); // removes un-needed "Inspect Accessibility Properties" on right-click
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false); // Settings>Home>Firefox Home Content>Recent Activity>Shortcuts>Sponsored shortcuts
+user_pref("browser.newtabpage.activity-stream.showSponsored", false); // Settings>Home>Firefox Home Content>Recent Activity>Recommended by Pocket>Sponsored Stories
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false); // Settings>Home>Firefox Home Content>Recent Activity>Bookmarks
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false); // Settings>Home>Firefox Home Content>Recent Activity>Most Recent Download
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false); // Settings>Home>Firefox Home Content>Recent Activity>Visited Pages
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false); // Settings>Home>Firefox Home Content>Recent Activity>Pages Saved to Pocket
+user_pref("browser.toolbars.bookmarks.visibility", "never"); // always hide bookmark bar
+user_pref("browser.startup.homepage_override.mstone", "ignore"); // What's New page after updates; master switch
+user_pref("browser.urlbar.suggest.history", false); // Browsing history; hide URL bar dropdown suggestions
+user_pref("browser.urlbar.suggest.bookmark", false); // Bookmarks; hide URL bar dropdown suggestions
+user_pref("browser.urlbar.suggest.openpage", false); // Open tabs; hide URL bar dropdown suggestions
+user_pref("browser.urlbar.suggest.topsites", false); // Shortcuts; disable dropdown suggestions with empty query
+user_pref("browser.urlbar.suggest.engines", false); // Search engines; tab-to-search
+user_pref("browser.urlbar.quicksuggest.enabled", false); // hide Firefox Suggest UI in the settings
+user_pref("browser.bookmarks.max_backups", 0); // minimize disk use; manually back-up
+user_pref("view_source.wrap_long_lines", true);  // wrap source lines
+user_pref("devtools.debugger.ui.editor-wrapping", true);  // wrap lines in devtools
+user_pref("browser.zoom.full", false); // text-only zoom, not all elements on page
+user_pref("layout.word_select.eat_space_to_next_word", false); // do not select the space next to a word when selecting a word
+user_pref("browser.tabs.loadBookmarksInTabs", true); // force bookmarks to open in a new tab, not the current tab
+user_pref("ui.key.menuAccessKey", 0); // remove underlined characters from various settings
+user_pref("general.autoScroll", false); // disable unintentional behavior for middle click
+user_pref("ui.SpellCheckerUnderlineStyle", 1); // [HIDDEN] dots for spell check errors
+user_pref("media.videocontrols.picture-in-picture.display-text-tracks.size", "small"); // PiP
+user_pref("media.videocontrols.picture-in-picture.urlbar-button.enabled", false); // PiP in address bar
+user_pref("reader.parse-on-load.enabled", false); // disable reader mode
+user_pref("network.trr.mode", 2); // enable TRR (with System fallback)
+user_pref("network.trr.max-fails", 5); // lower max attempts to use DoH
+user_pref("geo.provider.use_geoclue", false); // [LINUX]
+user_pref("pdfjs.defaultZoomValue", "page-width"); // PDF zoom level
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.visibility", "hide-sidebar");
+user_pref("sidebar.main.tools", "history");
+user_pref("sidebar.verticalTabs", true);
